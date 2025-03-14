2846bda4807fe44d8f50871ff2e6b65fabecc16f
Lancinater <zwordgize@gmail.com>
2025-03-14 12:39:33 +1100

    scene can now be loaded, however original map is not maintained, will make it save map status later

--- Assets/Scripts/Managers/SceneLoadManager.cs
+++ Assets/Scripts/Managers/SceneLoadManager.cs
@@ -1,13 +1,57 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
 
 public class SceneLoadManager : MonoBehaviour
 {
-    public void OnLoadRoomEvent(object data)
+    private AssetReference currentScene;
+    public AssetReference map;
+    
+    
+    /// <summary>
+    /// This method is called when the scene is loaded
+    /// </summary>
+    /// <param name="data"></param>
+    public async void OnLoadRoomEvent(object data)
     {
         if (data is RoomDataSO)
         {
             var currentData = (RoomDataSO)data;
-            Debug.Log("Loading room: " + currentData.roomType);
+            // Debug.Log("Loading room: " + currentData.roomType);
+
+            currentScene = currentData.sceneToLoad;
         }
+        
+        // Unload the current scene
+        await UnloadSceneTask();
+        // Load the scene
+        await LoadSceneTask();
+    }
+
+    /// <summary>
+    /// Asynchronously load the scene
+    /// </summary>
+    private async Awaitable LoadSceneTask()
+    {
+        var s =currentScene.LoadSceneAsync(LoadSceneMode.Additive);
+        await s.Task;
+        if(s.Status == AsyncOperationStatus.Succeeded)
+        {
+            SceneManager.SetActiveScene(s.Result.Scene);
+        }
+    }
+    
+    private async Awaitable UnloadSceneTask()
+    {
+        await SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+    }
+    
+    public async void LoadMap()
+    {
+        await UnloadSceneTask();
+
+        currentScene = map;
+        await LoadSceneTask();
     }
 }
--- Assets/Scripts/Room/MonoBehavior/FinishRoom.cs
+++ Assets/Scripts/Room/MonoBehavior/FinishRoom.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class FinishRoom : MonoBehaviour
 {
+    public ObjectEventSO loadMapEvent;
     private void OnMouseUp()
     {
         // Back to the map
-        
+        loadMapEvent.RaiseEvent(null, this);
     }
 }
